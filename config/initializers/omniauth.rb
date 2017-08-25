3234b7939f006ad470316ea861b4fd7cb0630658
wayne712 <weilong712@gmail.com>
2017-08-25 05:45:02 +0000

    fix weibo oauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :weibo, ENV['WEIBO_KEY'], ENV['WEIBO_SECRET']
+  provider :weibo, ENV['WEIBO_KEY'], ENV['WEIBO_SECRET'],
+    token_params: {redirect_uri: "http://yan13.cn/auth/weibo/callback" }
 end
