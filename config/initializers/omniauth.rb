057eb218079924a949660d9542f3a5128df9f1de
wayne712 <weilong712@gmail.com>
2018-07-18 08:11:10 +0000

    comment omniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :weibo, ENV['WEIBO_KEY'], ENV['WEIBO_SECRET'],
-    token_params: {redirect_uri: "http://yan13.cn/auth/weibo/callback" }
-end
+#Rails.application.config.middleware.use OmniAuth::Builder do
+#  provider :weibo, ENV['WEIBO_KEY'], ENV['WEIBO_SECRET'],
+#    token_params: {redirect_uri: "http://yan13.cn/auth/weibo/callback" }
+#end
